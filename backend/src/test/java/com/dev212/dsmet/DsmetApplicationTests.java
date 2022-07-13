a57311abb84c8fc05a81c5f065c7304594f7af32
Brutos212 <fabio212@hotmail.com>
2022-07-13 15:03:07 -0300

    Project created

---
+++ backend/src/test/java/com/dev212/dsmet/DsmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev212.dsmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
